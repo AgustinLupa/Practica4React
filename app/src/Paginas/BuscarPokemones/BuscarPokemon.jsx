2122f71fdfddb66daa2caaec28f79a7321a249de
agus lupa <agusluparini@gmail.com>
2023-10-08 17:15:21 -0300

    xd

--- app/src/Paginas/BuscarPokemones/BuscarPokemon.jsx
+++ app/src/Paginas/BuscarPokemones/BuscarPokemon.jsx
@@ -1,6 +1,11 @@
 const BuscarPokemon= (props)=> {
     return(
         <>
+            <div class="card">
+                <div class="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>
         </>
     )
 }
