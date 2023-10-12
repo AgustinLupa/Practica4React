df390b1e4dd11fc2b04ba07f6f786ccc5e8e400d
Ezequiel de la Fuente <133174623+DeLaFuenteEzequiel@users.noreply.github.com>
2023-10-12 13:53:09 -0300

    Login acomodado

--- app/src/Paginas/Usuarios/Usuarios.jsx
+++ app/src/Paginas/Usuarios/Usuarios.jsx
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
+import './Usuarios.css';
 
-const Usuario = () => {
+const Usuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
-  const [nuevoUsuario, setNuevoUsuario] = useState({ nombre: '', email: '' });
+  const [nuevoUsuario, setNuevoUsuario] = useState({ nombre: '', contraseña: '' }); // Cambié 'email' a 'contraseña'
   const [busqueda, setBusqueda] = useState('');
   const [resultadoBusqueda, setResultadoBusqueda] = useState([]);
 
   const handleCrearUsuario = (e) => {
     e.preventDefault();
-    
     setUsuarios([...usuarios, nuevoUsuario]);
-   
-    setNuevoUsuario({ nombre: '', email: '' });
+    setNuevoUsuario({ nombre: '', contraseña: '' }); // Cambié 'email' a 'contraseña'
   };
 
   const handleBuscarUsuarios = () => {
-    
     const resultados = usuarios.filter((usuario) =>
       usuario.nombre.toLowerCase().includes(busqueda.toLowerCase())
     );
@@ -23,49 +21,56 @@ const Usuario = () => {
   };
 
   return (
+    <div className="usuario-container">
+      <h1>Usuarios</h1>
 
-    
-    <div>
-      <h1>Admin Usuarios</h1>
-      
-      <h2>Crear Usuario</h2>
+      <h2 className="crear-usuario">Crear Usuario</h2>
       <form onSubmit={handleCrearUsuario}>
-        <label>
-          Nombre:
-          <input
-            type="text"
-            value={nuevoUsuario.nombre}
-            onChange={(e) => setNuevoUsuario({ ...nuevoUsuario, nombre: e.target.value })}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={nuevoUsuario.email}
-            onChange={(e) => setNuevoUsuario({ ...nuevoUsuario, email: e.target.value })}
-          />
-        </label>
-        <button type="submit">Crear</button>
+        <div className="input-container">
+          <label>
+            Nombre:
+            <input
+              type="text"
+              value={nuevoUsuario.nombre}
+              onChange={(e) => setNuevoUsuario({ ...nuevoUsuario, nombre: e.target.value })}
+              class="col-sm-8 col-form-label"
+            />
+          </label>
+        </div>
+        <div className="usuario-container">
+          <label>
+            Contraseña:
+            <input
+              type="password"
+              value={nuevoUsuario.contraseña}
+              onChange={(e) => setNuevoUsuario({ ...nuevoUsuario, contraseña: e.target.value })}
+              class="col-sm-8 col-form-label"
+            />
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">Crear</button>
       </form>
 
-      <h2>Buscar Usuarios</h2>
-      <input
-        type="text"
-        placeholder="Buscar por nombre"
-        value={busqueda}
-        onChange={(e) => setBusqueda(e.target.value)}
-      />
-      <button onClick={handleBuscarUsuarios}>Buscar</button>
+      <h2 className="buscar-usuario">Buscar Usuarios</h2>
+      <div className="inputnpm-container">
+        <input
+          type="text"
+          placeholder="Buscar por nombre"
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+          class="col-sm-2 col-form-label"
+        />
+      </div>
+      <button onClick={handleBuscarUsuarios} className="btn btn-primary">Buscar</button>
 
       <h3>Resultados de la Búsqueda</h3>
-      <ul>
+      <ul className="resultados-busqueda">
         {resultadoBusqueda.map((usuario) => (
-          <li key={usuario.email}>{usuario.nombre} - {usuario.email}</li>
+          <li key={usuario.contraseña}>{usuario.nombre} - {usuario.contraseña}</li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default Usuario;
+export default Usuarios;
